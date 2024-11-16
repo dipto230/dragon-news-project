e4394c98e1489b1b30a79e09eadcef1f07a79d2d
dipto230 <diptoroy230305@gmail.com>
2024-11-16 17:15:35 +0600

    time section done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
 
         </div>
         <h2 className="text-gray-500">Journalism Without Fear or Favour</h2>
+        <p>{moment().format("dddd, MMMM Do YYYY")}</p>
             
         </div>
     );
