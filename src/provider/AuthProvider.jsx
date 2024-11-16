94d3d661dbc85850e417f27177c316b1b7d0e865
dipto230 <diptoroy230305@gmail.com>
2024-11-17 04:53:21 +0600

    private route done

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -7,14 +7,18 @@ import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWith
 
 const AuthProvider = ({children}) => {
     const [user , setUser ] =useState(null);
-    console.log(user);
+    const [loading , setLoading]=useState(true);
+    console.log( loading,user);
     const createNewUser=(email,password)=>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
     const userLogin=(email, password)=>{
+        setLoading(true);
        return signInWithEmailAndPassword(auth, email, password);
     };
     const logOut=()=>{
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -24,11 +28,13 @@ const AuthProvider = ({children}) => {
         createNewUser,
         logOut,
         userLogin,
+        loading,
     };
 
     useEffect(()=>{
      const unSubscribe=   onAuthStateChanged(auth , currentUser=>{
             setUser(currentUser);
+            setLoading(false);
         });
         return()=>{
             unSubscribe();
