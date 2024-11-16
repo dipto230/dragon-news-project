95273ef0724ffdaf6be97eaa4e23280bfe36c575
dipto230 <diptoroy230305@gmail.com>
2024-11-16 18:31:24 +0600

    left aside section done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNavbar from '../components/layout-component/LeftNavbar';
 
 const HomeLayout = () => {
     return (
@@ -17,7 +18,9 @@ const HomeLayout = () => {
             <Navbar></Navbar>
             </nav>
             <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3'>
-                <aside className='left col-span-3'>Left navbar</aside>
+                <aside className='left col-span-3'>
+                    <LeftNavbar></LeftNavbar>
+                </aside>
                 <section className='col-span-6'>Main content</section>
                 <aside className=' col-span-3'>Right navbar</aside>
             </main>
