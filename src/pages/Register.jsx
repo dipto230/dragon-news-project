15c6bc5fe54c16956610bcd82e31556feaa01581
dipto230 <diptoroy230305@gmail.com>
2024-11-17 15:31:47 +0600

    final line

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,78 +1,122 @@
-import React, { useContext } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../provider/AuthProvider';
+
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+import { useContext, useState } from "react";
 
 const Register = () => {
-    const {createNewUser,  setUser , updateUserProfile}=useContext(AuthContext);
-    const Navigate =useNavigate();
-    const handleSubmit =(e)=>{
-e.preventDefault();
-const from =new FormData(e.target);
-const name =from.get("name");
-const email =from.get("email");
-const photo =from.get("photo");
-const password =from.get("password");
-console.log({name,photo,email,password});
-createNewUser(email,password)
-.then(result=>{
-const user = result.user;
-setUser(user);
-updateUserProfile({ displayName: name, photoURL: photo })
-.then(()=>{
-  Navigate("/");
+  const { createNewUser, setUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [error, setError] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //get form data
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    if (name.length < 5) {
+      setError({ ...error, name: "name should be more then 5 character" });
+    }
+    const email = form.get("email");
+    const photo = form.get("photo");
+    const password = form.get("password");
 
-}).catch(err=>{
-  console.log(err);
-});
+    createNewUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        updateUserProfile({ displayName: name, photoURL: photo })
+          .then(() => {
+            navigate("/");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        // ..
+      });
+  };
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-2xl font-semibold text-center">
+          Register your account
+        </h2>
+        <form onSubmit={handleSubmit} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {error.name && (
+            <label className="label text-sx text-red-500">{error.name}</label>
+          )}
 
-})
-.catch((error)=>{
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage);
-});
-    };
-    return (
-        <div className='min-h-screen flex justify-center items-center'>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl rounded-none p-10">
-        <h2  className="text-2xl font-semibold text-center">Register with your account </h2>
-     <form onSubmit={handleSubmit} className="card-body">
-     <div className="form-control">
-         <label className="label">
-           <span className="label-text">Photo-Url</span>
-         </label>
-         <input name="photo" type="photo-url" placeholder="photo-url" className="input input-bordered" required />
-       </div>
-     <div className="form-control">
-         <label className="label">
-           <span className="label-text">Name</span>
-         </label>
-         <input name="name" type="name" placeholder="name" className="input input-bordered" required />
-       </div>
-       <div className="form-control">
-         <label className="label">
-           <span className="label-text">Email</span>
-         </label>
-         <input name="email" type="email" placeholder="email" className="input input-bordered" required />
-       </div>
-       <div className="form-control">
-         <label className="label">
-           <span className="label-text">Password</span>
-         </label>
-         <input name="password" type="password" placeholder="password" className="input input-bordered" required />
-         <label className="label">
-           <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-         </label>
-       </div>
-       <div className="form-control mt-6">
-         <button className="btn btn-neutral rounded-none">Register</button>
-       </div>
-     </form>
-     <p className="font-semibold">Already have an account? {""}<Link className="text-red-500" to="/auth/login">Login</Link></p>
-   </div>
-           
-       </div>
-    );
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              name="photo"
+              placeholder="photo-url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* email input  */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          {error.register && <label className="label">{error.register}</label>}
+
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none">Register</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">
+          Already Have An Account ?{" "}
+          <Link className="text-red-500" to="/auth/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,53 +1,67 @@
-import { createContext, useEffect, useState } from "react";
-import app from "../firbase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 
- export const AuthContext = createContext();
- const auth = getAuth(app);
+import {createContext , useEffect, useState} from "react";
+import app from "../firebase/firebase.config.js";
+import {
 
-const AuthProvider = ({children}) => {
-    const [user , setUser ] =useState(null);
-    const [loading , setLoading]=useState(true);
-    console.log( loading,user);
-    const createNewUser=(email,password)=>{
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+// eslint-disable-next-line react/prop-types
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // console.log(loading, user);
+
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  const authInfo = {
+    user,
+    setUser,
+    createNewUser,
+    logOut,
+    userLogin,
+    loading,
+    updateUserProfile,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
     };
-    const userLogin=(email, password)=>{
-        setLoading(true);
-       return signInWithEmailAndPassword(auth, email, password);
-    };
-    const logOut=()=>{
-        setLoading(true);
-        return signOut(auth);
-    }
-    const updateUserProfile=({updatedData})=>{
-        return updateProfile(auth.currentUser , updatedData)
+  }, []);
 
-    }
-
-    const authInfo={
-        user,
-        setUser,
-        createNewUser,
-        logOut,
-        userLogin,
-        loading,
-        updateUserProfile,
-    };
-
-    useEffect(()=>{
-     const unSubscribe=   onAuthStateChanged(auth , currentUser=>{
-            setUser(currentUser);
-            setLoading(false);
-        });
-        return()=>{
-            unSubscribe();
-        };
-    },[])
-    return (
-        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
